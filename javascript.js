af5fb87256f12b433057e629e1e988661a238f13
Alexander <alexander.soriano001@gmail.com>
2022-05-07 09:25:16 +0800

    operator and equal buttons now work as intended

--- javascript.js
+++ javascript.js
@@ -26,44 +26,45 @@ function operate(operator,a,b) {
     } return
 }
 
-function displayFn() {
-        displayValue += this.textContent;
-        display.textContent = displayValue;
-}
-
 let displayValue = '';
+let firstValue = '';
+let secondValue = '';
+let operator = '';
+let arrayValue = [];
 
-
-const display = document.querySelector('.display')
-display.textContent=displayValue
+const display = document.querySelector('.display');
 
 const numberButtons = document.querySelectorAll('.number');
 numberButtons.forEach((numberButton) => {
-    numberButton.addEventListener('click',displayFn)
-});
-
-let operator;
-const operatorButtons = document.querySelectorAll('.operator');
-operatorButtons.forEach((operatorButton) => {
-    operatorButton.addEventListener('click',displayFn)
-    operatorButton.addEventListener('click',() => {
-        operator=operatorButton.textContent
+    numberButton.addEventListener('click', () => {
+        displayValue += numberButton.textContent
+        display.textContent = displayValue
     })
-});
-
-let firstValue;
-let secondValue;    
-function getValues() {
-    let splitDisplay = () => displayValue.split(/[+\,*\,/\,-]+/);
-    splitValue = splitDisplay();
-    firstValue = Number(splitValue[0]);
-    secondValue = Number(splitValue[1]);
-}
-const equalButton = document.querySelector('.equal');
-equalButton.addEventListener('click', () => {
-    getValues();
-    display.textContent=operate(operator,firstValue,secondValue)
-    
 })
 
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener('click', () => {
+        if (firstValue==='' || operator===''){
+            firstValue = display.textContent;
+            operator = operatorButton.textContent;
+            displayValue = '';
+        } else {
+            secondValue = display.textContent;
+            let result = operate(operator,Number(firstValue),Number(secondValue));
+            display.textContent = result;
+            firstValue = result;
+            displayValue = '';
+            operator = operatorButton.textContent;
+        }
+    })
+})
 
+const equalButton = document.querySelector('.equal');
+equalButton.addEventListener('click', () => {
+    secondValue = display.textContent;
+    display.textContent = operate(operator,Number(firstValue),Number(secondValue));
+    displayValue = '';
+    firstValue = display.textContent
+    operator = ''
+})
